3d3f686b5cee0895d69e7d960ac034d92a7e483d
JUan Andrade <jfandradea@gmail.com>
2021-09-10 15:19:23 -0500

    finish scroll to accept exercise

---
+++ scroll-to-accept/scroll-to-accept.js
@@ -0,0 +1,26 @@
+function scrollToAccept() {
+  const terms = document.querySelector('.terms-and-conditions');
+  if (!terms) {
+    return;
+  }
+  const button = document.querySelector('.accept');
+
+  function obCallback(payload) {
+    if (payload[0].intersectionRatio === 1) {
+      button.disabled = false;
+      // stop observing the button
+      ob.unobserve(terms.lastElementChild);
+    } /* else {
+      button.disabled = true;
+    } */
+  }
+  const ob = new IntersectionObserver(obCallback, {
+    root: terms,
+    threshold: 1,
+  });
+  ob.observe(terms.lastElementChild);
+  /* terms.addEventListener('scroll', function(e) {
+    console.log(e);
+  }); */
+}
+scrollToAccept();
